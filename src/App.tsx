006544ab98f6658268ba6277ffc93f0c80de9c45
Nalin <nalinssn@gmail.com>
2023-02-04 19:27:12 +0530

    tapjacking issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
+import {Image, DeviceEventEmitter} from 'react-native';
 // import Login from './screens/Login';
 import Home from './screens/Home';
 
 const App = () => {
+  const [show, setShow] = useState(false);
+  const onFocus = (params: {appHasFocus: any}) => {
+    setShow(!params.appHasFocus);
+  };
+  const subscription = DeviceEventEmitter.addListener('focusChange', onFocus);
+
+  useEffect(() => {
+    subscription.remove();
+  }, [subscription]);
+  if (show) {
+    console.log('log', show);
+    return (
+      <Image
+        source={require('../src/values/assets/image/bgImages.png')}
+        style={{flex: 1}}
+      />
+    );
+  }
   return <Home />;
 };
 
