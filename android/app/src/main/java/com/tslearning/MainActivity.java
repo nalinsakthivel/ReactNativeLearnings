c7feb0ffdaf6f71a8ccc952367035e65f5086817
Nalin Sakthivel <nalinssn@gmail.com>
2023-02-06 20:10:30 +0530

    Tap jacking issue fixed

--- android/app/src/main/java/com/tslearning/MainActivity.java
+++ android/app/src/main/java/com/tslearning/MainActivity.java
@@ -1,19 +1,12 @@
 package com.tslearning;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
-
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends ReactActivity {
-  private ImageView imageView;
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -21,19 +14,15 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() { return "TSLearning"; }
+ 
   @Override
-  public void onWindowFocusChanged(boolean hasFocus) {
-    super.onWindowFocusChanged(hasFocus);
-    if (getReactNativeHost().getReactInstanceManager().getCurrentReactContext() != null) {
-      WritableMap params = Arguments.createMap();
-      params.putBoolean("appHasFocus", hasFocus);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-      getReactNativeHost().getReactInstanceManager()
-              .getCurrentReactContext()
-              .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-              .emit("focusChange", params);
-    }
-
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
   }
 
   /**
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-import {Image, DeviceEventEmitter} from 'react-native';
 // import Login from './screens/Login';
 import Home from './screens/Home';
+import {withSecurityScreen} from './Security';
 
 const App = () => {
-  const [show, setShow] = useState(false);
-  const onFocus = (params: {appHasFocus: any}) => {
-    setShow(!params.appHasFocus);
-  };
-  const subscription = DeviceEventEmitter.addListener('focusChange', onFocus);
-
-  useEffect(() => {
-    subscription.remove();
-  }, [subscription]);
-  if (show) {
-    console.log('log', show);
-    return (
-      <Image
-        source={require('../src/values/assets/image/bgImages.png')}
-        style={{flex: 1}}
-      />
-    );
-  }
   return <Home />;
 };
 
-export default App;
+export default withSecurityScreen(App);
