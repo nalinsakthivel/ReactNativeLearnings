94c0c28152c0dedd8a3272bae7fe5358f07b4b37
Nalin Sakthivel <66259386+nalinsakthivel@users.noreply.github.com>
2023-04-05 14:32:18 +0530

    Update HttpsClient.ts

--- src/httpsClient/HttpsClient.ts
+++ src/httpsClient/HttpsClient.ts
@@ -14,3 +14,32 @@ const HttpClient = async (url: string, method: Method, data: any) => {
 };
 
 export default HttpClient;
+
+<!-- import useSWR, { mutate } from 'swr';
+
+// Fetch the data from the API
+const { data, error } = useSWR('/api/data', fetcher);
+
+// Define a function to handle an update to the data
+const handleUpdateData = async () => {
+  const response = await fetch('/api/data', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ newData: 'updated data' })
+  });
+
+  if (response.ok) {
+    // Update the cached data with the new data
+    mutate('/api/data');
+  }
+};
+
+// Render the component
+return (
+  <div>
+    {data && <p>{data}</p>}
+    <button onClick={handleUpdateData}>Update data</button>
+  </div>
+); -->
