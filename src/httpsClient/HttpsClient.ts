e6af2636a8175635417dc1e31939bcf912657358
Nalin Sakthivel <66259386+nalinsakthivel@users.noreply.github.com>
2023-04-05 14:34:45 +0530

    https client updated

--- src/httpsClient/HttpsClient.ts
+++ src/httpsClient/HttpsClient.ts
@@ -1,4 +1,4 @@
-import axios, {Method} from 'axios';
+import axios, { Method } from "axios";
 
 const HttpClient = async (url: string, method: Method, data: any) => {
   var options: any = {};
@@ -6,8 +6,8 @@ const HttpClient = async (url: string, method: Method, data: any) => {
   options.method = method;
   options.data = data;
 
-  console.log('URL', url);
-  console.log('options', JSON.stringify(options));
+  console.log("URL", url);
+  console.log("options", JSON.stringify(options));
 
   const response = await axios(url, options);
   return response;
@@ -15,31 +15,23 @@ const HttpClient = async (url: string, method: Method, data: any) => {
 
 export default HttpClient;
 
-<!-- import useSWR, { mutate } from 'swr';
+// import useSWR, { mutate } from 'swr';
 
-// Fetch the data from the API
-const { data, error } = useSWR('/api/data', fetcher);
+// // Fetch the data from the API
+// const { data, error } = useSWR('/api/data', fetcher);
 
-// Define a function to handle an update to the data
-const handleUpdateData = async () => {
-  const response = await fetch('/api/data', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ newData: 'updated data' })
-  });
+// // Define a function to handle an update to the data
+// const handleUpdateData = async () => {
+//   const response = await fetch('/api/data', {
+//     method: 'PUT',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ newData: 'updated data' })
+//   });
 
-  if (response.ok) {
-    // Update the cached data with the new data
-    mutate('/api/data');
-  }
-};
-
-// Render the component
-return (
-  <div>
-    {data && <p>{data}</p>}
-    <button onClick={handleUpdateData}>Update data</button>
-  </div>
-); -->
+//   if (response.ok) {
+//     // Update the cached data with the new data
+//     mutate('/api/data');
+//   }
+// };
